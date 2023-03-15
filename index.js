b48ac2182424aeeacb0c3183774e7ed67f966b3a
Pavlina Glozygova <pglozygova@gmail.com>
2023-03-15 20:50:56 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('funguju!');
+
+let hadejCislo = 1
+const kostka = document.querySelector('.dice')
+const otocKostkou = () => {
+	hadejCislo = hadejCislo + 1
+	if (hadejCislo === 7) {
+		hadejCislo = 1
+	}
+	kostka.src = `img/side${hadejCislo}.svg`
+}
+document.addEventListener('keydown', otocKostkou)
